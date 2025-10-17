1ef18a32d7160c97d07622b572f165db372186c2
saniaezzati <saniaezzati1384@gmail.com>
2025-10-18 00:15:54 +0200

    feat: add edit status and edit task functions

--- todolist/core/services/task_service.py
+++ todolist/core/services/task_service.py
@@ -31,5 +31,47 @@ class TaskService:
         task = Task.create(title=title, description=description, deadline=deadline)
         proj.add_task(task)
         return task
+    
+    def edit_task(
+        self,
+        project_id: str,
+        task_id: str,
+        title: Optional[str] = None,
+        description: Optional[str] = None,
+        status: Optional[str] = None,
+        deadline: Optional[date] = None,
+    ) -> Task:
+        proj = self.storage.get_project(project_id)
+        if proj is None:
+            raise InvalidEntityError("Project not found.")
+        task = proj.get_task(task_id)
+        if task is None:
+            raise InvalidEntityError("Task not found.")
+
+        if title is not None:
+            validate_task_title(title)
+            task.title = title
+        if description is not None:
+            validate_task_description(description)
+            task.description = description
+        if status is not None:
+            validate_status(status)
+            task.status = status
+        if deadline is not None:
+            validate_deadline(deadline)
+            task.deadline = deadline
+
+        return task
+    
+    def change_status(self, project_id: str, task_id: str, new_status: str) -> Task:
+        proj = self.storage.get_project(project_id)
+        if proj is None:
+            raise InvalidEntityError("Project not found.")
+        task = proj.get_task(task_id)
+        if task is None:
+            raise InvalidEntityError("Task not found.")
+        validate_status(new_status)
+        task.status = new_status
+        return task
 
     

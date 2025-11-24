c5a3c047e051ee62e35edc575d99768efb8b958a
saniaezzati <saniaezzati1384@gmail.com>
2025-11-25 02:48:08 +0330

    feat: update main function with database

--- todolist/main.py
+++ todolist/main.py
@@ -1,12 +1,24 @@
-from __future__ import annotations
-
+from todolist.db.session import SessionLocal
+from todolist.repositories.sqlalchemy_project_repository import SqlAlchemyProjectRepository
+from todolist.repositories.sqlalchemy_task_repository import SqlAlchemyTaskRepository
+from todolist.services.project_service import ProjectService
+from todolist.services.task_service import TaskService
 from todolist.cli.menu import CLI
 
+def create_app_cli():
+    db = SessionLocal()
+    project_repo = SqlAlchemyProjectRepository(db)
+    task_repo = SqlAlchemyTaskRepository(db)
+    project_service = ProjectService(project_repo, max_projects=None)
+    task_service = TaskService(task_repo, max_tasks=None)
+    return CLI(project_service, task_service), db
 
-def main() -> None:
-    cli = CLI()
-    cli.run()
-
+def main():
+    cli, db = create_app_cli()
+    try:
+        cli.run()
+    finally:
+        db.close()
 
 if __name__ == "__main__":
     main()

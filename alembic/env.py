7bdbbfacc46c762a8a4194d12b68006e2bb9a5e9
saniaezzati <saniaezzati1384@gmail.com>
2025-11-25 03:00:17 +0330

    feat: add alembic environment files

---
+++ alembic/env.py
@@ -0,0 +1,38 @@
+from logging.config import fileConfig
+import os
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+from alembic import context
+from dotenv import load_dotenv
+from todolist.db.base import Base
+from todolist.models.project import Project
+from todolist.models.task import Task
+
+load_dotenv()
+config = context.config
+fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+def get_url():
+    return os.getenv("DATABASE_URL")
+
+def run_migrations_offline():
+    url = get_url()
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section), prefix="sqlalchemy.", url=get_url(), poolclass=pool.NullPool
+    )
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
